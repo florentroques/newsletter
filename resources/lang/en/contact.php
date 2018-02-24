6abf236662701bc6335dd7c2042a31e90d5d1c2a
florentroques <florent.open@gmail.com>
2018-02-24 05:33:27 +0100

    fixed weird characters issue

--- resources/lang/en/contact.php
+++ resources/lang/en/contact.php
@@ -1,31 +1,31 @@
 <?php
 
 return [
-         'title' => 'Contacts',
-     'description' => 'Closer to us',
-     'email' => [
-             'send' => [
-                 'subject' => ': subject of: name',
-             'greeting' => 'Hello,: name',
-             'intro' => [
+         'title' => 'Contacts',
+     'description' => 'Closer to us',
+     'email' => [
+             'send' => [
+                 'subject' => ': subject of: name',
+             'greeting' => 'Hello,: name',
+             'intro' => [
 
-                ],
-             'outro' => [
-                     'Please reply this to respond to the pesa',
-             ],
-         ],
-     ],
-     'form' => [
-             'name' => 'Full Name',
-         'email' => 'Email Address',
-         'subject' => 'Subject',
-         'message' => 'Messages',
-     ],
-     'button' => [
-             'send' => 'Send Message'
-     ],
-     'message' => [
-             'sent' => 'Thank you. Message successfully sent. ',
-         'failedToSend' => 'The message failed to send. Please try again. ',
-     ],
+                ],
+             'outro' => [
+                     'Please reply this to respond to the pesa',
+             ],
+         ],
+     ],
+     'form' => [
+             'name' => 'Full Name',
+         'email' => 'Email Address',
+         'subject' => 'Subject',
+         'message' => 'Messages',
+     ],
+     'button' => [
+             'send' => 'Send Message'
+     ],
+     'message' => [
+             'sent' => 'Thank you. Message successfully sent. ',
+         'failedToSend' => 'The message failed to send. Please try again. ',
+     ],
 ];|
--- resources/lang/en/page.php
+++ resources/lang/en/page.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-         'error' => [
-             'somethingError' => 'Oops! There is an error',
-         'backHome' => 'Home',
-         'backPrev' => 'Previous Page',
-     ],
+         'error' => [
+             'somethingError' => 'Oops! There is an error',
+         'backHome' => 'Home',
+         'backPrev' => 'Previous Page',
+     ],
 ];
